7f966b6bea1c5f86c21e41ca975194b069e22be3
roto21 <angel.guitarno@gmail.com>
2017-01-29 17:55:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+info="<HTML>
+<body bgcolor=&#34;gray&#34;> 
+<marquee><font size=&#34;20&#34; color=&#34;gree&#34;>:roto2:</font></marquee>
+<center><img src=&#34;http://www.roto2.com/superroto2.png&#34;></center>
+<marquee><font size=&#34;20&#34; color=&#34;gree&#34;>:roto2:</font></marquee>
+</body>
+</HTML>"
+
+document.write(info)
+</script>
