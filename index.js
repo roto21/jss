f6ac1512fb51d13e7ef551f4abd564723d199382
roto21 <angel.guitarno@gmail.com>
2017-01-29 17:58:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<script>
-info="<HTML>
-<body bgcolor=&#34;gray&#34;> 
-<marquee><font size=&#34;20&#34; color=&#34;gree&#34;>:roto2:</font></marquee>
-<center><img src=&#34;http://www.roto2.com/superroto2.png&#34;></center>
-<marquee><font size=&#34;20&#34; color=&#34;gree&#34;>:roto2:</font></marquee>
-</body>
-</HTML>"
+:roto2:
 
-document.write(info)
-</script>
+
+jaja salu2
